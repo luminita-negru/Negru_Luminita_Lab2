25051a8e698d037159702c23dbe30e60cafa3db5
LUMINIȚA NEGRU <luminita.negru@stud.ubbcluj.ro>
2023-10-31 00:56:18 +0200

    Add project files.

---
+++ Data/Negru_Luminita_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Negru_Luminita_Lab2.Models;
+
+namespace Negru_Luminita_Lab2.Data
+{
+    public class Negru_Luminita_Lab2Context : DbContext
+    {
+        public Negru_Luminita_Lab2Context (DbContextOptions<Negru_Luminita_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Negru_Luminita_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Negru_Luminita_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Negru_Luminita_Lab2.Models.Author>? Author { get; set; }
+    }
+}
